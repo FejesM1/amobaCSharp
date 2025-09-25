cbc0c35f7032aef081d426cb8dc52499de25aa05
FMáté <fejesm@kkszki.hu>
2025-09-25 10:51:21 +0200

    Add project files.

---
+++ amoba/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amoba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
