4ab088a29bbda396bdf88ab7c81d476f23433414
FMáté <fejesm@kkszki.hu>
2025-09-25 13:16:42 +0200

    koordináták

--- amoba/Program.cs
+++ amoba/Program.cs
@@ -20,9 +20,16 @@ namespace amoba
                 }
             }
 
+           
             Kiir();
 
             Console.ReadLine();
+            Console.WriteLine("Adja meg az elso koordinátát: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adja meg a második koordinátát: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+           
         }
         static void Kiir()
         {
@@ -36,17 +43,34 @@ namespace amoba
                     Console.Write(palya[c, o]);
                     Console.Write('|');
                 }
-            
-            Console.WriteLine();
-            if (c != 9)
+
+                Console.WriteLine();
+                if (c != 9)
+                {
+                    Console.WriteLine("├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤");
+
+                }
+                else
+                {
+                    Console.WriteLine("└───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘");
+                }
+            }
+            void Lep(int x, int y, string jel)
             {
-                Console.WriteLine("├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤");
+                if (x < 0 || x >= 10 || y < 0 || y >= 10)
+                {
+                    Console.WriteLine("Érvénytelen koordináta");
+                }
+                if (palya[x, y] == "   ")
+                {
+                    palya[x, y] = jel;
+                }
+                else
+                {
+                    Console.WriteLine("Ez a mező már foglalt!");                
+                }
 
             }
-            else
-            {
-                    Console.WriteLine("└───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘");
-            }
-            }
-    }  }
-}
+        }
+    }
+}   
